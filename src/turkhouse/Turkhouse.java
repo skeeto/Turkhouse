4892f2ef5c0ce245ca0fff91b40c8ddc21ca026a
Christopher Wellons <mosquitopsu@gmail.com>
2011-10-30 11:34:38 -0400

    Fill out some documentation.

--- src/turkhouse/Turkhouse.java
+++ src/turkhouse/Turkhouse.java
@@ -9,14 +9,14 @@ import java.util.Random;
 public class Turkhouse extends java.awt.Robot implements Runnable {
 
     private static final Random RNG = new Random();
+    private static final int SECOND = 1000;
 
+    /* Relevant screen positions. */
     private static final Point SEARCH = new Point(800, 188);
     private static final Point BUYOUT = new Point(760, 560);
     private static final Point ACCEPT = new Point(770, 220);
     private static final Point SILVER = new Point(810, 270);
     private static final Point GOLD = new Point(772, 270);
-
-    private static final int SECOND = 1000;
 
     public static void main(String[] args) {
         try {
@@ -38,7 +38,7 @@ public class Turkhouse extends java.awt.Robot implements Runnable {
             click(SEARCH);
             delay((int) (1.5 * SECOND));
             while (isCheap()) {
-                System.out.println("looks cheap, buying");
+                System.out.println("Looks cheap, buying.");
                 click(BUYOUT);
                 click(ACCEPT);
                 delay(1 * SECOND);
@@ -49,23 +49,41 @@ public class Turkhouse extends java.awt.Robot implements Runnable {
         }
     }
 
+    /**
+     * Click a point on the screen.
+     * @param p  the point to click
+     */
     private void click(Point p) {
         mouseMove(p.x, p.y);
         mousePress(InputEvent.BUTTON1_MASK);
         mouseRelease(InputEvent.BUTTON1_MASK);
     }
 
+    /**
+     * Sample the color at a screen point.
+     * @param p  the point to be sampled
+     * @return the color at the sampled point
+     */
     private Color sample(Point p) {
         Rectangle pixel = new Rectangle(p.x, p.y, 1, 1);
         return new Color(createScreenCapture(pixel).getRGB(0, 0));
     }
 
+    /**
+     * Determines if the current top item is less than 1 silver.
+     * @return true if the current top AH item is cheap
+     */
     private boolean isCheap() {
         Color silver = sample(SILVER);
         Color gold = sample(GOLD);
         return (average(gold) < 127) && (average(silver) < 127);
     }
 
+    /**
+     * Determine the average color value.
+     * @param c  the color to work with
+     * @return the average color value
+     */
     private int average(Color c) {
         return (c.getRed() + c.getGreen() + c.getBlue()) / 3;
     }
